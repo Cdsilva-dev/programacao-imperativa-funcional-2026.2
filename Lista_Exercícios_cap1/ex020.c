22fcb11bbb2c5c9604f29e4937f8d20cad6690f9
Cdsilva-dev <cdsilvaoliveira840@gmail.com>
2026-08-23 00:00:28 -0300

    Exercícios

---
+++ Lista_Exercícios_cap1/ex020.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+ printf("\xC9\xBB\n");
+ printf("\xC8\xBC\n");
+ printf("\xCD\xBA\n");
+
+    return 0;
+}
---
+++ Lista_Exercícios_cap1/ex021.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+/*Primeira versão*/
+printf("Treinamento em programacao.\nLinguagem C.\n");
+/*Segunda versão*/
+printf("Treinamento em programacao.\n");
+printf("Linguagem C.\n");
+/*Terceira versão*/
+printf("\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB\n");
+    printf("\xBATreinamento em programacao.\xBA\n");
+    printf("\xBALinguagem C.               \xBA\n"); 
+    printf("\xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC\n");
+    return 0;
+}
